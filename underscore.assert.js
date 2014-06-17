8d1722fc1ae83594fe0182f80494e3c865634918
Piotr Kowalski <piecioshka@gmail.com>
2014-06-17 14:29:15 +0200

    Fixed comments - be more accoring with Docker

--- underscore.assert.js
+++ underscore.assert.js
@@ -1,27 +1,34 @@
 /**
  * @author Piotr Kowalski <piecioshka@gmail.com>
  * @fileOverview Underscore.js assertion helper
- * @see https://github.com/piecioshka/underscore.assert.js
- * @requires {@link http://underscorejs.org/|Underscore.js}
+ * @see https://github.com/piecioshka/underscore.assert
+ * @requires http://underscorejs.org/
  * @license The MIT License
- * @example
- *   _.assert(true, 'True must be truly value'); 
- *   // idle...
- *   _.assert(typeof Object === 'number', 'Global value *Object* should be fn');
- *   // throws AssertionError with message
  */
+
+// Example of _.assert
+// -------------------
+
+//     _.assert(true, 'True must be truly value'); // idle...
+//     _.assert(typeof Object === 'number', 'Global value *Object* should be fn'); // throws AssertionError with message
+
 /*jslint nomen: true, indent: 4 */
-/*global _ */
-;(function (global) {
+
+(function (root, factory) {
     'use strict';
 
-    var _ = global._;
+    if (typeof define !== 'undefined' && define.amd) {
+        define(['underscore'], factory);
+    } else {
+        factory(root._);
+    }
+}(this, function (_) {
+    'use strict';
 
     /**
-     * Assertion Error.
+     * Assertion Error module.
+     *
      * @param {string} [message]
-     * @class
-     * @constructor
      */
     function AssertionError(message) {
         this.name = 'AssertionError';
@@ -32,11 +39,12 @@
     AssertionError.prototype.constructor = AssertionError;
 
     /**
-     * Check first param for *true*.
-     * When first param is *false* value, throw {AssertionError}.
-     * @param {Boolean} condition
+     * Check first param for `true`.
+     * When first param is `false` value, throw `AssertionError`.
+     *
+     * @param {boolean} condition
      * @param {string} [message]
-     * @throws {AssertionError} Condition must be truly.
+     * @throws When condition will be falsy value.
      */
     function assert(condition, message) {
         if (!condition) {
@@ -47,4 +55,4 @@
     _.mixin({
         'assert': assert
     });
-}(this));
+}));
